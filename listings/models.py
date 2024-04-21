2747fad85fe6169bdb7850946f825a0eec3acbd4
Eudes Fourier AYENA <ayena2fourier@gmail.com>
2024-04-21 08:14:45 +0100

    commit du 21042024 08:13

--- listings/models.py
+++ listings/models.py
@@ -10,12 +10,22 @@ class Brand(models.Model):
 
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(null=False, choices=Genre.choices, max_length=5)
-    biography = models.fields.CharField(null=True, max_length=1000)
+    biography = models.fields.CharField(max_length=1000)
     year_formed = models.fields.IntegerField(validators=[MinValueValidator(1990), MaxValueValidator(2021)])
     active = models.fields.BooleanField(default=True)
     official_homepage = models.fields.URLField(null=True, blank=True)
 
 
 class Listing(models.Model):
-    title =  models.fields.CharField(max_length=100)
 
+    class Type(models.TextChoices):
+        Records = 'RCD'
+        Clothing = 'CLT'
+        Posters = 'PST'
+        Miscellaneous = 'MSN'
+
+    title =  models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=1000)
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(max_length=5)
+    type = models.fields.CharField(choices=Type.choices, max_length=5)
